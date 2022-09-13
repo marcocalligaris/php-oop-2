79845d6944c621e8685bef08fae584aaf5388eac
marcocalligaris <marcocally@gmail.com>
2022-09-13 21:30:42 +0200

    Added address

---
+++ models/shipping/Address.php
@@ -0,0 +1,142 @@
+<?php
+
+class Address 
+{
+    private $firstname;
+    private $lastname;
+    private $street;
+    private $zipcode;
+    private $city;
+    private $country;
+
+    public function __construct($firstname, $lastname, $street, $zipcode, $city, $country){
+        $this->setFirstname($firstname);
+        $this->setLastname($lastname);
+        $this->setStreet($street);
+        $this->setZipcode($zipcode);
+        $this->setCity($city);
+        $this->setCountry($country);
+    }
+
+    /**
+     * Get the value of firstname
+     */ 
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set the value of firstname
+     *
+     * @return  self
+     */ 
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastname
+     */ 
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set the value of lastname
+     *
+     * @return  self
+     */ 
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of street
+     */ 
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set the value of street
+     *
+     * @return  self
+     */ 
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of zipcode
+     */ 
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * Set the value of zipcode
+     *
+     * @return  self
+     */ 
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of city
+     */ 
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set the value of city
+     *
+     * @return  self
+     */ 
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of country
+     */ 
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set the value of country
+     *
+     * @return  self
+     */ 
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
+
+?>
