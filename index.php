ad41040f97cfcfb6d5aa64564c582980e7c6497b
marcocalligaris <marcocally@gmail.com>
2022-09-13 17:14:19 +0200

    Created item

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php 
+include_once __DIR__ . '/models/products/FoodProduct.php';
+
+$item = new FoodProduct('Cibo umido in scatola', 'Per cani', 1.80, ['cani'], ['carne di manzo', 'verdura'], '20-12-2022');
+
+var_dump($item);
 
 ?>
